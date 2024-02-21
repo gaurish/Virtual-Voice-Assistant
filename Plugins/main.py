9cd57f377532fd1e89f5f5a0efded88f9bf2b55c
Gaurish Sharma <contact@gaurishsharma.com>
2024-02-20 23:26:33 -0600

    Update requirements.txt and add Dockerfile

--- Plugins/main.py
+++ Plugins/main.py
@@ -1,28 +1,28 @@
-try:
-    # importing prebuilt modules
-    import os
-    import logging
-    import pyttsx3
-    logging.disable(logging.WARNING)
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disabling warnings for gpu requirements
-    from keras_preprocessing.sequence import pad_sequences
-    import numpy as np
-    from keras.models import load_model
-    from pickle import load
-    import speech_recognition as sr
-    import sys
-    #sys.path.insert(0, os.path.expanduser('~') + "/PycharmProjects/Virtual_Voice_Assistant")
-    sys.path.insert(0, os.path.expanduser('~')+"/Virtual-Voice-Assistant") # adding voice assistant directory to system path
-    # importing modules made for assistant
-    from database import *
-    from image_generation import generate_image
-    from gmail import *
-    from API_functionalities import *
-    from system_operations import *
-    from browsing_functionalities import *
-except (ImportError, SystemError, Exception, KeyboardInterrupt) as e:
-    print("ERROR OCCURRED WHILE IMPORTING THE MODULES")
-    exit(0)
+# try:
+# importing prebuilt modules
+import os
+import logging
+import pyttsx3
+logging.disable(logging.WARNING)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disabling warnings for gpu requirements
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.models import load_model
+from pickle import load
+import speech_recognition as sr
+import sys
+#sys.path.insert(0, os.path.expanduser('~') + "/PycharmProjects/Virtual_Voice_Assistant")
+sys.path.insert(0, os.path.expanduser('~')+"/Virtual-Voice-Assistant") # adding voice assistant directory to system path
+# importing modules made for assistant
+from database import *
+from image_generation import generate_image
+from gmail import *
+from API_functionalities import *
+from system_operations import *
+from browsing_functionalities import *
+# except (ImportError, SystemError, Exception, KeyboardInterrupt) as e:
+#     print("ERROR OCCURRED WHILE IMPORTING THE MODULES")
+#     exit(0)
 
 '''
 import os
--- setup.py
+++ setup.py
@@ -22,48 +22,48 @@ table = '''CREATE TABLE IF NOT EXISTS ASSISTANT (SERIAL_NO INTEGER PRIMARY KEY,
 cursor.execute(table)
 conn.commit()
 
-try:
+# try:
     # importing prebuilt modules
-    import pyttsx3
-    from keras_preprocessing.sequence import pad_sequences
-    import numpy as np
-    from keras.models import load_model
-    from pickle import load
-    import speech_recognition as sr
-    import sys
-    from keras.preprocessing.text import Tokenizer
-    from keras_preprocessing.sequence import pad_sequences
-    from sklearn.preprocessing import LabelEncoder
-    from tensorflow.python.keras.models import Sequential
-    from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
-    import datetime
-    from dotenv import load_dotenv
-    from newsapi import NewsApiClient
-    import re
-    import requests
-    from wolframalpha import Client
-    import webbrowser
-    import wikipedia
-    import speedtest
-    from youtubesearchpython import VideosSearch
-    import smtplib
-    import io
-    import warnings
-    from PIL import Image
-    from stability_sdk import client
-    import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
-    import math
-    import psutil
-    import time
-    from random import randint
-    import AppOpener
-    from pynput.keyboard import Key, Controller
-    from PIL import ImageGrab
-    import wmi
-except ImportError:
-    print("MODULES NOT INSTALLED!")
-    exit(0)
-except KeyboardInterrupt:
-    print("INTERRUPTED WHILE IMPORTING MODULES")
+import pyttsx3
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.models import load_model
+from pickle import load
+import speech_recognition as sr
+import sys
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+import datetime
+from dotenv import load_dotenv
+from newsapi import NewsApiClient
+import re
+import requests
+from wolframalpha import Client
+import webbrowser
+import wikipedia
+import speedtest
+from youtubesearchpython import VideosSearch
+import smtplib
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import math
+import psutil
+import time
+from random import randint
+# import AppOpener
+# from pynput.keyboard import Key, Controller
+from PIL import ImageGrab
+# import wmi
+# except ImportError:
+#     print("MODULES NOT INSTALLED!")
+#     exit(0)
+# except KeyboardInterrupt:
+#     print("INTERRUPTED WHILE IMPORTING MODULES")
 
 print("\n\nSETUP SUCCESSFUL")
